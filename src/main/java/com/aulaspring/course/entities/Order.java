d8250e631f38520ffdc98a99213eda2b6d4b2a72
MarianaOlvr11 <marxvitoria@hotmail.com>
2024-06-17 17:47:22 -0300

    feat:OrderStatus enum.

--- src/main/java/com/aulaspring/course/entities/Order.java
+++ src/main/java/com/aulaspring/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.aulaspring.course.entities;
 
 
+import com.aulaspring.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -24,13 +25,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client,OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -55,6 +59,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
